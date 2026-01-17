2337e12ed0444c4a1c4edf7e67613c17954dfd17
Truong Van Thanh <truongthanh160588@gmail.com>
2026-01-17 11:06:18 +0700

    chore: update course video links 01-20

--- data/course.ts
+++ data/course.ts
@@ -46,10 +46,34 @@ const lessons: Lesson[] = Array.from({ length: 20 }, (_, i) => {
     "Xuất file và định dạng audio",
   ];
 
+  // YouTube video IDs cho 20 bài học
+  const youtubeIds = [
+    "R52ZxgJk-YY", // lesson01: https://youtu.be/R52ZxgJk-YY?si=vqoWus96LFQJzNe2
+    "F8mqpi11nHE", // lesson02: https://www.youtube.com/watch?v=F8mqpi11nHE
+    "I5tYhPQ9YhM", // lesson03: https://www.youtube.com/watch?v=I5tYhPQ9YhM
+    "6mJ99ZrbZDg", // lesson04: https://www.youtube.com/watch?v=6mJ99ZrbZDg
+    "rRAiIgN8z0U", // lesson05: https://www.youtube.com/watch?v=rRAiIgN8z0U
+    "1gwDfiLkScQ", // lesson06: https://youtu.be/1gwDfiLkScQ?si=WAkpFKp66YxTeYUK
+    "IpOlESzJdmc", // lesson07: https://www.youtube.com/watch?v=IpOlESzJdmc
+    "8H2qZwHKhok", // lesson08: https://www.youtube.com/watch?v=8H2qZwHKhok
+    "2punFs5Vfzw", // lesson09: https://www.youtube.com/watch?v=2punFs5Vfzw
+    "2qly_PGguO0", // lesson10: https://youtu.be/2qly_PGguO0?si=HvypPB0CW0knuqOc
+    "ViG7HxlvXr8", // lesson11: https://youtu.be/ViG7HxlvXr8?si=upC2joYzFG4WTNCc
+    "cbY3LUMHFD4", // lesson12: https://youtu.be/cbY3LUMHFD4?si=oRFLfOAJNWqrwCXY
+    "TDOvufi7XWU", // lesson13: https://youtu.be/TDOvufi7XWU?si=BtXBGVshP9-Af8o8
+    "IV6JFhruLSg", // lesson14: https://youtu.be/IV6JFhruLSg?si=rjFB6tRfPSs027Y0
+    "7M1QrTbDRkM", // lesson15: https://youtu.be/7M1QrTbDRkM?si=jU60D1OtM57sJcrT
+    "34PaidrgV_E", // lesson16: https://youtu.be/34PaidrgV_E?si=88x_jLicWEfWYSed
+    "CVemH0to_0s", // lesson17: https://youtu.be/CVemH0to_0s?si=WMQySLrAd_-xeynv
+    "10qorbnMaWg", // lesson18: https://youtu.be/10qorbnMaWg?si=-qqQOuCqEhPPsdFW
+    "lgDsVG3tdU4", // lesson19: https://youtu.be/lgDsVG3tdU4?si=mI9ldKgblJz7KU5z
+    "mOd3WgwEOlA", // lesson20: https://youtu.be/mOd3WgwEOlA?si=4uBvdY5SZqGosQRe
+  ];
+
   return {
     id: `lesson${String(lessonNum).padStart(2, "0")}`,
     title: `Bài ${String(lessonNum).padStart(2, "0")}: ${titles[i]}`,
-    youtubeId: "dQw4w9WgXcQ", // Placeholder - thay sau
+    youtubeId: youtubeIds[i],
     is_preview: lessonNum <= 3, // 3 bài đầu là preview
     quiz: Array.from({ length: 5 }, (_, q) => ({
       id: `q${q + 1}`,
